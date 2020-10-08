c252f21007213a4038f3cfbcaeb285af5e96257c
rugshubina <shubinalullaby@gmail.com>
2020-10-08 12:42:19 +0300

    manifest

---
+++ app/src/main/java/com/gshubina/lullzzz/testappkotlin/client/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.gshubina.lullzzz.testappkotlin.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
