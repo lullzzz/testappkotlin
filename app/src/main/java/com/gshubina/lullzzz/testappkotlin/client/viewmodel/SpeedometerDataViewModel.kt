4c07907cef37681ef74952cc517e062ce7cef7d6
rugshubina <shubinalullaby@gmail.com>
2020-10-08 13:39:03 +0300

    view models

---
+++ app/src/main/java/com/gshubina/lullzzz/testappkotlin/client/viewmodel/SpeedometerDataViewModel.kt
@@ -0,0 +1,17 @@
+package com.gshubina.lullzzz.testappkotlin.client.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SpeedometerDataViewModel : ViewModel() {
+    private val mSpeedometerData : MutableLiveData<Double> = MutableLiveData()
+
+    fun receive(item : Double) {
+        mSpeedometerData.value = item
+    }
+
+    fun getSpeedometerData() : LiveData<Double> {
+        return mSpeedometerData
+    }
+}
---
+++ app/src/main/java/com/gshubina/lullzzz/testappkotlin/client/viewmodel/TachometerDataViewModel.kt
@@ -0,0 +1,17 @@
+package com.gshubina.lullzzz.testappkotlin.client.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TachometerDataViewModel : ViewModel() {
+    private val mTachometerData : MutableLiveData<Double> = MutableLiveData()
+
+    fun receive(item : Double) {
+        mTachometerData.value = item
+    }
+
+    fun getTachometerData() : LiveData<Double> {
+        return mTachometerData
+    }
+}
